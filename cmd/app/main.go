8ebc3f954c9ea20f1ac699d5f818452736f96be7
Krokozabra213 <super.fetisov228@list.ru>
2026-01-28 10:52:50 +0300

    added config

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,24 +1,28 @@
 package main
 
 import (
-	"log"
-	"time"
+	"fmt"
 
-	postgresclient "github.com/Krokozabra213/test_api/pkg/database/postgres-client"
+	"github.com/Krokozabra213/test_api/internal/config"
 )
 
 func main() {
-	pgConfig := postgresclient.NewPGConfig("localhost", "5432", "postgres", "secret", "mydb", "disable",
-		25, 5, 5*time.Minute)
-	db, err := postgresclient.New(pgConfig)
+	cfg, err := config.Init("configs/main.yml", ".env")
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
+	fmt.Printf("%+v\n", cfg)
+	// pgConfig := postgresclient.NewPGConfig("localhost", "5432", "postgres", "secret", "mydb", "disable",
+	// 	25, 5, 5*time.Minute)
+	// db, err := postgresclient.New(pgConfig)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	log.Println("Connected to PostgreSQL")
+	// log.Println("Connected to PostgreSQL")
 
-	// Database Shutdown
-	if err := db.Shutdown(); err != nil {
-		log.Printf("shutdown error: %v", err)
-	}
+	// // Database Shutdown
+	// if err := db.Shutdown(); err != nil {
+	// 	log.Printf("shutdown error: %v", err)
+	// }
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,162 @@
 package config
+
+import (
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"github.com/spf13/viper"
+)
+
+// Default values
+const (
+	defaultHTTPHost               = "localhost"
+	defaultHTTPPort               = "8080"
+	defaultHTTPWriteTimeout       = 10 * time.Second
+	defaultHTTPReadTimeout        = 10 * time.Second
+	defaultHTTPMaxHeaderMegabytes = 1
+
+	defaultLimiterRPS   = 10
+	defaultLimiterBurst = 2
+	defaultLimiterTTL   = 10 * time.Minute
+
+	defaultSSLMode         = "disable"
+	defaultMaxOpenConns    = 25
+	defaultMaxIdleConns    = 5
+	defaultConnMaxLifetime = 5 * time.Minute
+)
+
+type (
+	Config struct {
+		App      AppConfig
+		HTTP     HTTPConfig
+		Postgres PostgresConfig
+		Limiter  LimiterConfig
+	}
+
+	LimiterConfig struct {
+		RPS   int
+		Burst int
+		TTL   time.Duration
+	}
+
+	AppConfig struct {
+		AppSecretKey string
+		Environment  string
+	}
+
+	PostgresConfig struct {
+		Host            string
+		Port            string
+		User            string
+		Password        string
+		DBName          string
+		SSLMode         string        `mapstructure:"sslMode"`
+		MaxOpenConns    int           `mapstructure:"maxOpenConns"`
+		MaxIdleConns    int           `mapstructure:"maxIdleConns"`
+		ConnMaxLifetime time.Duration `mapstructure:"connMaxLifetime"`
+	}
+
+	HTTPConfig struct {
+		Host               string        `mapstructure:"host"`
+		Port               string        `mapstructure:"port"`
+		ReadTimeout        time.Duration `mapstructure:"readTimeout"`
+		WriteTimeout       time.Duration `mapstructure:"writeTimeout"`
+		MaxHeaderMegabytes int           `mapstructure:"maxHeaderBytes"`
+	}
+)
+
+func newCfg() Config {
+	cfg := Config{
+		App:      AppConfig{},
+		Limiter:  LimiterConfig{},
+		Postgres: PostgresConfig{},
+		HTTP:     HTTPConfig{},
+	}
+	return cfg
+}
+
+// Init loads config from file and environment variables.
+// Priority: defaults -> config file -> env vars
+func Init(configfile, envfile string) (*Config, error) {
+	populateDefault()
+
+	if err := parseConfigFile(configfile); err != nil {
+		return nil, err
+	}
+
+	cfg := newCfg()
+
+	if err := unmarshal(&cfg); err != nil {
+		return nil, err
+	}
+
+	if err := setFromEnv(envfile, &cfg); err != nil {
+		return nil, err
+	}
+	return &cfg, nil
+}
+
+func populateDefault() {
+	// http config
+	viper.SetDefault("http.host", defaultHTTPHost)
+	viper.SetDefault("http.port", defaultHTTPPort)
+	viper.SetDefault("http.maxHeaderMegabytes", defaultHTTPMaxHeaderMegabytes)
+	viper.SetDefault("http.readTimeout", defaultHTTPReadTimeout)
+	viper.SetDefault("http.writeTimeout", defaultHTTPWriteTimeout)
+
+	// limiter config
+	viper.SetDefault("limiter.rps", defaultLimiterRPS)
+	viper.SetDefault("limiter.burst", defaultLimiterBurst)
+	viper.SetDefault("limiter.ttl", defaultLimiterTTL)
+
+	// postgres config
+	viper.SetDefault("postgres.sslMode", defaultSSLMode)
+	viper.SetDefault("postgres.maxOpenConns", defaultMaxOpenConns)
+	viper.SetDefault("postgres.maxIdleConns", defaultMaxIdleConns)
+	viper.SetDefault("postgres.connMaxLifetime", defaultConnMaxLifetime)
+}
+
+func parseConfigFile(configPath string) error {
+	viper.SetConfigFile(configPath)
+	if err := viper.ReadInConfig(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// setFromEnv loads sensitive data from environment variables
+func setFromEnv(envpath string, cfg *Config) error {
+	err := godotenv.Load(envpath)
+	if err != nil {
+		return err
+	}
+
+	cfg.App.Environment = os.Getenv("ENV")
+	cfg.App.AppSecretKey = os.Getenv("APP_SECRET")
+
+	cfg.Postgres.Host = os.Getenv("POSTGRES_HOST")
+	cfg.Postgres.Port = os.Getenv("POSTGRES_PORT")
+	cfg.Postgres.User = os.Getenv("POSTGRES_USER")
+	cfg.Postgres.DBName = os.Getenv("POSTGRES_DB")
+	cfg.Postgres.Password = os.Getenv("POSTGRES_PASSWORD")
+
+	return nil
+}
+
+func unmarshal(cfg *Config) error {
+	if err := viper.UnmarshalKey("http", &cfg.HTTP); err != nil {
+		return err
+	}
+
+	if err := viper.UnmarshalKey("limiter", &cfg.Limiter); err != nil {
+		return err
+	}
+
+	if err := viper.UnmarshalKey("postgres", &cfg.Postgres); err != nil {
+		return err
+	}
+
+	return nil
+}
